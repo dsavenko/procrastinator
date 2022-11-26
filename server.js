83822706395da420dafbc5c7c9f60628a4a90650
Dmitry Savenko <ds@dsavenko.com>
2022-11-26 16:00:09 +0700

    Remove Web app, add new static site

--- server.js
+++
@@ -1,6 +0,0 @@
-const static = require('node-static')
-const file = new static.Server()
-
-require('http').createServer((request, response) => {
-    request.addListener('end', () => file.serve(request, response)).resume()
-}).listen(process.env.PORT || 8000)
